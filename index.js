74f4d578189277a17fecc54d14c08123c6c4d345
Rolex Omondi <rolexomondi46@gmail.com>
2023-03-14 22:22:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function() {
+    let num1 = 2000;
+    let num2 = 100;
+    return num1/num2;
+}
+divide()
+
+const square = x => x **2;
+square(2);
+
+const add = (a, b) => a+b;
+add(3, 4);
